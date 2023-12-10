8932d0fda6009ce26284922002a602e07f466146
AmazingAJ10 <allenjohnhobbs5792@gmail.com>
2023-12-10 13:13:12 -0500

    go

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "subs2": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
